f2b02d92af508f106ac480de3ea55836a884896f
kangmahfud <makhfudzamhari@gmail.com>
2021-11-13 15:36:42 +0700

    migration

---
+++ database/migrations/2021_11_13_082728_create_checkouts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('camp_id');
+            $table->string('card_number',16);
+            $table->date('expired_date');
+            $table->string('cvc',100);
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+
+            $table->foreign('camp_id')
+                ->references('id')
+                ->on('camps')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}

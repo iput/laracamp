4da7852ac353bfe1371bf9674f93bc088c424dcb
kangmahfud <makhfudzamhari@gmail.com>
2021-11-13 22:18:25 +0700

    slicing landing, checkout dan login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',function()
+{
+    return view('front.login');
+})->name('login');
+
+Route::get('/checkout',function()
+{
+    return view('front.checkout');
+})->name('checkout');
